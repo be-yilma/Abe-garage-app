643bc99d35c2250b0ca8bb36ca401610ce80dc4b
bezuayehu <bezuayehu87@gmail.com>
2024-03-23 20:49:21 -0400

    setting up bacend and frontend folder structure

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Abegarage main app</h1>
+      </div>
+      <div>
+        <p>Hello there</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
