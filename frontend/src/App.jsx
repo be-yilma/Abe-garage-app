14a2c2f9c74385e222c1c9c460da344537fe6e7b
bezuayehu <bezuayehu87@gmail.com>
2024-04-06 21:07:55 -0400

    Implementing login and addEmployee page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,29 @@
+// Import the css files
+import "./assets/template_assets/css/bootstrap.css";
+import "./assets/template_assets/css/style.css";
+import "./assets/template_assets/css/responsive.css";
+import "./assets/template_assets/css/color.css";
+
+// Import the custom css file
+import "./assets/styles/custom.css";
+import Footer from "./markup/components/Footer/Footer";
+import Header from "./markup/components/Header/Header";
+import Error from "./markup/pages/Error";
+import Home from "./markup/pages/Home";
+import Login from "./markup/pages/Login";
+import AddEmployee from "./markup/pages/admin/AddEmployee";
+import { Routes, Route } from "react-router-dom";
 function App() {
   return (
     <>
-      <div>
-        <h1>Abegarage main app</h1>
-      </div>
-      <div>
-        <p>Hello there</p>
-      </div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/admin/add-employee" element={<AddEmployee />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
---
+++ frontend/src/markup/pages/Error.jsx
@@ -0,0 +1,12 @@
+const Error = () => {
+  return (
+    <div>
+      <h1>
+        Sorry, the page you are looking for could not be found. Please go back
+        to the <a href="/">home page</a> and try again.
+      </h1>
+    </div>
+  );
+};
+
+export default Error;
