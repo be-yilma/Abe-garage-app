8957ce68ec0724b31d616effddfecde251bbdeb0
bezuayehu <bezuayehu87@gmail.com>
2024-10-30 11:50:07 -0400

    authrization from the backend using middlewares

--- backend/routes/employee.route.js
+++ backend/routes/employee.route.js
@@ -5,9 +5,15 @@ const router = express.Router();
 
 // import the employee controller
 const employeeController = require("../controllers/employee.controller");
+// import middleware
+const authMiddleware = require("../middlewares/auth.middleware");
 
 // pass the employee request to the employee controller and call the employee function in it
-router.post("/api/employee", employeeController.createEmployee);
+router.post(
+  "/api/employee",
+  [authMiddleware.verifyToken, authMiddleware.isAdmin],
+  employeeController.createEmployee
+);
 
 // exprot the router
 module.exports = router;
